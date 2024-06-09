d8d6bae72cdfab5a3c7b6ba58a31e65d2d035be2
Brady Joslin <brady@bradyjoslin.com>
2024-06-08 21:36:50 -0500

    feat: expression emails with resend

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	_ "embed"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/bitfield/script"
@@ -20,6 +23,8 @@ import (
 	"github.com/expr-lang/expr"
 	extism "github.com/extism/go-sdk"
 	"github.com/spf13/cobra"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
 	"gopkg.in/yaml.v3"
 )
 
@@ -52,7 +57,7 @@ type Task struct {
 
 const (
 	configFileName = "config.yaml"
-	version        = "0.2.2"
+	version        = "0.2.3"
 )
 
 var (
@@ -314,6 +319,61 @@ func callExtismPlugin(source string, function string, input string) (string, err
 	return response, nil
 }
 
+func resend(to string, from string, subject string, body string) error {
+	var html bytes.Buffer
+
+	gm := goldmark.New(
+		goldmark.WithExtensions(
+			extension.Linkify,
+			extension.Strikethrough,
+			extension.Table,
+		),
+	)
+	_ = gm.Convert([]byte(body), &html)
+
+	escapedHTML := strconv.QuoteToASCII(html.String())
+	escapedHTML = escapedHTML[1 : len(escapedHTML)-1] // Remove the extra double quotes
+
+	payload := []byte(fmt.Sprintf(`{
+	        "from": "%s",
+	        "to": "%s",
+	        "subject": "%s",
+	        "html": "%s"
+	    }`, from, to, subject, escapedHTML))
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://api.resend.com/emails", bytes.NewBuffer(payload))
+
+	if err != nil {
+		return err
+	}
+
+	apiKey := os.Getenv("RESEND_API_KEY")
+	if apiKey == "" {
+		return errors.New("RESEND_API_KEY is not set")
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("error sending email \n status code: %d\n%s", res.StatusCode, string(bodyBytes))
+	}
+
+	return nil
+}
+
 func runExpr(input string, expression string) (string, error) {
 	env := map[string]interface{}{
 		"input":      input,
@@ -321,6 +381,7 @@ func runExpr(input string, expression string) (string, error) {
 		"AppendFile": appendFile,
 		"ReadFile":   readfile,
 		"Extism":     callExtismPlugin,
+		"Resend":     resend,
 	}
 
 	program, err := expr.Compile(expression, expr.Env(env))
